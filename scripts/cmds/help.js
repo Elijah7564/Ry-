be0aea4fbc860253df3aaafbb3744f25897529b4
Elijah7564 <151037138+Elijah7564@users.noreply.github.com>
2024-03-27 07:57:23 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -53,13 +53,13 @@ msg+=`\n┃『${category.toUpperCase()}45𝗰𝗺𝗱𝘀』`;
             const cmds = names.slice(i, i + 1).map((item) => `━🕸️━${item}`);
             msg += `\n┃ ${cmds.join(" ".repeat(Math.max(1, 10 - cmds.join("").length)))}`;
           }
-          msg +=`\n╰━━━━━━━━━━━╯`;
+          msg +=`\n╰━━━━━━━━━━━╯/n꧁𝗘𝗟𝗜𝗔𝗦𝗕𝗔𝗥𝗨𝗧꧂`;
         }
       });
 
       const totalCommands = commands.size;
 
-      msg += `꧁𝗘𝗟𝗜𝗔𝗦𝗕𝗔𝗥𝗨𝗧꧂`;
+      msg += ``;
 
 msg+=``;
 
